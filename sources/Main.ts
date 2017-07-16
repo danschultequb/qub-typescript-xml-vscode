40f1bf24ce47a42cbecfbfdd77996b012fcb77c6
Dan Schulte <daschult@microsoft.com>
2017-07-15 23:07:56 -0700

    Prepare for release

--- sources/Main.ts
+++ sources/Main.ts
@@ -1,17 +1,12 @@
-// 'use strict';
+'use strict';
 
-// import * as vscode from "vscode";
+import * as vscode from "vscode";
+import * as qubVSCode from "qub-vscode/vscode";
 
-// import * as msbuild from "qub-msbuild";
+import * as xml from "./Extension";
 
-// import { Application, ApplicationInsightsTelemetry } from "../../Common/sources/VSCode";
-
-// // this method is called when your extension is activated
-// // your extension is activated the very first time the command is executed
-// export function activate(context: vscode.ExtensionContext) {
-//     const application = new Application();
-//     application.setRemoteTelemetryCreator(() => new ApplicationInsightsTelemetry({
-//         instrumentationKey: "6d6e08cd-67e6-47d8-88b8-700f000a1215"
-//     }));
-//     context.subscriptions.push(new msbuild.Extension(application));
-// }
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(new xml.Extension(new qubVSCode.Platform()));
+}
